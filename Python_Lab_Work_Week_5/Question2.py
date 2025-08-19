844b321738d44065e410d7b5aa47a65c973ee55c
iajay006 <ajay07503@gmail.com>
2025-08-19 15:12:48 +0530

    Update Question2.py
    insertion in between

--- Python_Lab_Work_Week_5/Question2.py
+++ Python_Lab_Work_Week_5/Question2.py
@@ -1,11 +1,19 @@
 def Question2():
     my_dict = {'a': 1, 'b': 2}
     print("Original:", my_dict)
-    # Insert
-    my_dict['c'] = 3
-    print("After Insertion:", my_dict)
-    # Delete
-    del my_dict['b']
-    print("After Deletion:", my_dict)
+
+    # Insert 'c':3 between 'a' and 'b'
+    new_dict = {}
+    for key, value in my_dict.items():
+        new_dict[key] = value
+        if key == 'a':
+            new_dict['c'] = 3
+
+    print("After Insertion:", new_dict)
+
+    # Delete 'b'
+    del new_dict['b']
+    print("After Deletion:", new_dict)
     print()
+
 Question2()
